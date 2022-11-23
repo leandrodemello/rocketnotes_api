9f1855ffb2c5f5d4210656e6273e7b8f7097a8c1
leandro <leandro.devmcz@gmail.com>
2022-11-23 19:53:31 -0300

    creat-post-routes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
+const routes = require("./routes")
+
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send("Hello, Leandro!")
-
-});
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
