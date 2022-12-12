bdd999741c76d451e57a5ecdbfea112a77d7cb3c
leandro <leandro.devmcz@gmail.com>
2022-12-12 20:44:30 -0300

    pack

--- src/database/knex/migrations/20221212231227_createNotes.js
+++ src/database/knex/migrations/20221212231227_createNotes.js
@@ -1,9 +1,13 @@
 
-exports.up = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("notes", table => {
+  table.increments("id");
+  table.text("title");
+  table.text("description");
+  table.integer("user_is").references("id").inTable("users");
+
+  table.timestamp("created_at").default(knex.fn.now());
+  table.timestamp("updated_at").default(knex.fn.now());
+});
 
 
-exports.down = function(knex) {
-  
-};
+exports.down = knex => knex.schema.dropTable("notes");
