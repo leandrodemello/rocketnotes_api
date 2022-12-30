1c5514cfab271937d1188a1a8dae574230fcb6af
leandro <leandro.devmcz@gmail.com>
2022-12-30 11:30:42 -0300

    uh

--- knexfile.js
+++ knexfile.js
@@ -5,16 +5,16 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
     pool: {
-      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb) //função para deletar as tags ao deletar uma nota.
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb), //função para deletar as tags ao deletar uma nota.
     },
     migrations: {
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations"),
     },
 
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
 
 };
