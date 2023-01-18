8bd1f9d3dbe68ea2001753e526ba2fd6f69b77f5
leandro <leandro.devmcz@gmail.com>
2023-01-18 10:08:16 -0300

    validação de usuário

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,17 @@
+const knex = require("../database/knex"); //IMPORTADA A CONECÇÃO COM O BANCO DE DADOS
+const AppError = require("../utils/AppError") // IMPORTAR O APPERROR PARA EVENTUAIS ERROS OU DIVERGENCIA DE DADOS
+
 class SessionsController {
    async create(request, response) {
       const { email, password } = request.body;
 
-      return response.json({ email, password });
+      const user = await knex("users").where({ email }).first();
+
+      if (!user) {
+         throw new AppError("E-mail e/ou senha incorreta", 401);
+      }
+
+      return response.json(user);
    }
 }
 
