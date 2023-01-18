3aba0d8210fe5b4df6b4f52c635a0148961ff8e0
leandro <leandro.devmcz@gmail.com>
2023-01-18 10:46:34 -0300

    com

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,13 +1,20 @@
 const knex = require("../database/knex"); //IMPORTADA A CONECÇÃO COM O BANCO DE DADOS
 const AppError = require("../utils/AppError") // IMPORTAR O APPERROR PARA EVENTUAIS ERROS OU DIVERGENCIA DE DADOS
+const { compare } = require("bcryptjs");  //COMPARAR SE A SENHA E A CORRETA DO USUÁRIO
+
+
 
 class SessionsController {
    async create(request, response) {
       const { email, password } = request.body;
 
       const user = await knex("users").where({ email }).first();
-
       if (!user) {
+         throw new AppError("E-mail e/ou senha incorreta", 401);
+      }
+
+      const passwordMatched = await compare(password, user.password);
+      if (!passwordMatched) {
          throw new AppError("E-mail e/ou senha incorreta", 401);
       }
 
