a641b13c08c76c9247184745ca433795befb996e
leandro <leandro.devmcz@gmail.com>
2023-01-29 17:13:23 -0300

    user

--- src/server.js
+++ src/server.js
@@ -34,4 +34,6 @@ app.use((err, request, response, next) => {
 })
 
 const PORT = 3333
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`)
+});
