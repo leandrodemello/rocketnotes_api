f399f4ac181de52df1e3992ebd6c705608f99f67
leandro <leandro.devmcz@gmail.com>
2023-03-16 10:20:57 -0300

    test

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -85,7 +85,7 @@ class NotesController {
 
     const userTags = await knex("tags").where({ user_id });
     const notesWithTags = notes.map(note => {
-      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+      const noteTags = userTags.filter(tag => tag.note_id);
 
       return {
         ...note,
